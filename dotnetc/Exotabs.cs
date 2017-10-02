5e85289d628b59b5cedc5c50ae382a1303914d87
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2017-10-02 16:07:20 +0200

    renew exotabs

--- dotnetc/Exotabs.cs
+++ dotnetc/Exotabs.cs
@@ -5,35 +5,48 @@ using System;
 using System.Collections;
 //Directive for collections "List"
 using System.Collections.Generic;
-namespace dotnetc {
+namespace dotnetc
+{
 
-    class ExoTabs {
+
+    //Ajout de modifs
+    class ExoTabs
+    {
         // Créer un tableau de 10 cases avec la valeur de 0
-        public void CreateArray (int nbVal) {
+        public void CreateArray(int nbVal)
+        {
             int[] tab = new int[nbVal];
-            for (int i = 0; i < tab.Length; i++) {
+            for (int i = 0; i < tab.Length; i++)
+            {
                 tab[i] = 0;
-
+                Console.WriteLine(tab[i]);
             }
         }
+
+        // Seconde méthode pour créer un tableau de 10 cases avec 0
 
         // Créer un tableau de 10 cases rempli avec les valeurs de ses indices 
 
-        public void CreateArray1 (int nbVal1) {
+        public void CreateArray1(int nbVal1)
+        {
             int[] tab1 = new int[nbVal1];
-            for (int i = 0; i < tab1.Length; i++) {
+            for (int i = 0; i < tab1.Length; i++)
+            {
                 tab1[i] = i;
-                Console.WriteLine (tab1[i]);
+                Console.WriteLine(tab1[i]);
             }
         }
+        // Note Random r = new Random(DateTime.Now.Milisecond); seed
 
         // Créer un tableau de 10 cases rempli avec des valeurs aléatoires 
-        public void CreateArray2 (int nbVal2, int min, int max) {
+        public void CreateArray2(int nbVal2, int min, int max)
+        {
             int temp;
             int[] tab2 = new int[nbVal2];
-            Random random = new Random ();
-            for (int i = 0; i < tab2.Length; i++) {
-                tab2[i] = random.Next (min, max); //valeur comprise entre 0  et 10 (min,max)
+            Random random = new Random();
+            for (int i = 0; i < tab2.Length; i++)
+            {
+                tab2[i] = random.Next(min, max); //valeur comprise entre 0  et 10 (min,max)
             }
 
             // Déplacer le tableau dans une liste et Sort()
@@ -45,9 +58,12 @@ namespace dotnetc {
             }*/
 
             //Tri
-            for (int k = 0; k < tab2.Length; ++k) {
-                for (int j = 1; j < tab2.Length - k; ++j) {
-                    if (tab2[j - 1] > tab2[j]) {
+            for (int k = 0; k < tab2.Length; ++k)
+            {
+                for (int j = 1; j < tab2.Length - k; ++j)
+                {
+                    if (tab2[j - 1] > tab2[j])
+                    {
                         temp = tab2[j - 1];
                         tab2[j - 1] = tab2[j];
                         tab2[j] = temp;
@@ -55,50 +71,69 @@ namespace dotnetc {
                 }
             }
             // Vérification
-            for (int y = 0; y < tab2.Length; y++) {
-                Console.WriteLine (tab2[y]);
+            for (int y = 0; y < tab2.Length; y++)
+            {
+                Console.WriteLine(tab2[y]);
             }
 
         }
         //List 
-        public void CreateList () {
-            Random rand = new Random ();
-            var values = new List<int> ();
-            for (int i = 0; i < 100; i++) {
-                values.Add (rand.Next ());
+        public void CreateList()
+        {
+            Random rand = new Random();
+            var values = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                values.Add(rand.Next());
             }
-            values.Sort ();
-            foreach (var item in values) {
-                Console.WriteLine (item);
+            values.Sort();
+            foreach (var item in values)
+            {
+                Console.WriteLine(item);
             }
         }
 
         // Créer une hashtable
-        public void CreateHash () {
-            Hashtable hash = new Hashtable ();
-            hash.Add ("Paris", 120000);
-            hash.Add ("Montpellier", 110000);
-            hash.Add ("Bordeaux", 1000);
-            hash.Add ("Nantes", 100);
-            hash.Add ("NulPart", 163000);
-            foreach (DictionaryEntry key in hash) {
-                if ((int) key.Value > 10000) {
-                    Console.WriteLine (key.Key + " " + key.Value);
+        public void CreateHash()
+        {
+            Hashtable hash = new Hashtable();
+            hash.Add("Paris", 120000);
+            hash.Add("Montpellier", 110000);
+            hash.Add("Bordeaux", 1000);
+            hash.Add("Nantes", 100);
+            hash.Add("NulPart", 163000);
+            foreach (DictionaryEntry key in hash)
+            {
+                if ((int)key.Value > 10000)
+                {
+                    Console.WriteLine(key.Key + " " + key.Value);
                 }
             }
         }
 
+        public void CreateDic()
+        {
+            Dictionary<string, float> annuaire = new Dictionary<string, float>();
+            annuaire.Add("Personne1", 15);
+            annuaire.Add("Personne2", 18);
+            annuaire.Add("Personne3", 25);
+            foreach (KeyValuePair<string, float> k in annuaire)
+                Console.WriteLine(k.Key + " " + k.Value);
+        }
+
         //  Créer une variable de type string de 5 lettres aléatoires
         // Utilisation de chars[] et convert to string
-        public void CreateStringF (int nbL) {
-            Random rnd = new Random ();
+        public void CreateStringF(int nbL)
+        {
+            Random rnd = new Random();
             string alpha = "abcdefghijklmnopqrstuvwxyz";
             char[] myWord = new char[nbL];
-            for (int i = 0; i < nbL; i++) {
-                myWord[i] = alpha[rnd.Next (alpha.Length)];
+            for (int i = 0; i < nbL; i++)
+            {
+                myWord[i] = alpha[rnd.Next(alpha.Length)];
             }
             string word = new string(myWord);
-            Console.WriteLine (word);
+            Console.WriteLine(word);
         }
 
     }
